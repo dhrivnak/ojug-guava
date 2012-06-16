19364dee818eaf8adc52616518bde15a9336fa23
Dan Hrivnak <dhrivnak@gmail.com>
2012-06-16 13:31:46 -0500

    Finish CollectionsDemo.

--- src/main/java/hrivnak/CollectionsDemo.java
+++ src/main/java/hrivnak/CollectionsDemo.java
@@ -9,7 +9,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.BiMap;
 import com.google.common.collect.BoundType;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashBiMap;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultiset;
 import com.google.common.collect.ImmutableSet;
@@ -18,15 +21,34 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.Sets;
 import com.google.common.collect.SortedMultiset;
+import com.google.common.collect.Table;
 import com.google.common.collect.TreeMultiset;
 
 public class CollectionsDemo {
 	public static void main(String[] args) {
+		lists();
 		sets();
 		multiSets();
 		sortedMultiSets();
 		maps();
-		lists();
+		biMaps();
+		tables();
+	}
+
+	private static void lists() {
+		List<ClassWithSuperLongClassNameToHelpProveMyPoint> regularList = new ArrayList<ClassWithSuperLongClassNameToHelpProveMyPoint>();
+		List<ClassWithSuperLongClassNameToHelpProveMyPoint> guavaList = Lists.newArrayList();
+
+		List<String> forward = Lists.newArrayList("we", "can", "initialize", "lists", "here");
+		List<String> backwardView = Lists.reverse(forward);
+
+		printCollection("list:", forward);
+		printCollection("list reversed:", backwardView);
+
+		// let's see what happens when we remove an element from the backwardView
+		backwardView.remove("initialize");
+		printCollection("list:", forward);
+		printCollection("list reversed:", backwardView);
 	}
 
 	private static void sets() {
@@ -74,20 +96,29 @@ public class CollectionsDemo {
 		Map<String, Integer> handyForUnitTests = ImmutableMap.of("one", 1, "two", 2, "three", 3);
 	}
 
-	private static void lists() {
-		List<ClassWithSuperLongClassNameToHelpProveMyPoint> regularList = new ArrayList<ClassWithSuperLongClassNameToHelpProveMyPoint>();
-		List<ClassWithSuperLongClassNameToHelpProveMyPoint> guavaList = Lists.newArrayList();
+	private static void biMaps() {
+		BiMap<String, String> colorsToFlavors = HashBiMap.create();
+		colorsToFlavors.put("blue", "blueberry");
+		colorsToFlavors.put("orange", "orange");
+		colorsToFlavors.put("red", "cherry");
+		colorsToFlavors.put("green", "apple");
 
-		List<String> forward = Lists.newArrayList("we", "can", "initialize", "lists", "here");
-		List<String> backwardView = Lists.reverse(forward);
+		BiMap<String, String> flavorsToColors = colorsToFlavors.inverse(); // this gives us a view, not a new map
+		System.out.println("flavorsToColors.get(\"apple\") = " + flavorsToColors.get("apple"));
+		System.out.println("colorsToFlavors.get(\"red\") = " + colorsToFlavors.get("red"));
+		System.out.println("calling flavorsToColors.remove(\"blueberry\"): " + flavorsToColors.remove("blueberry"));
+		System.out.println("colorsToFlavors.get(\"blue\") = " + colorsToFlavors.get("blue"));
+	}
 
-		printCollection("list:", forward);
-		printCollection("list reversed:", backwardView);
+	private static void tables() {
+		// http://code.google.com/p/guava-libraries/wiki/NewCollectionTypesExplained#Table
+		Table<Integer, Integer, String> table = HashBasedTable.create();
+		table.put(1, 2, "row1,col2");
+		table.put(1, 3, "row1,col3");
+		table.put(2, 3, "row2,col3");
 
-		// let's see what happens when we remove an element from the backwardView
-		backwardView.remove("initialize");
-		printCollection("list:", forward);
-		printCollection("list reversed:", backwardView);
+		table.row(1); // returns a Map mapping 2 to "row1,col2", 3 to "row1,col3"
+		table.column(3); // returns a Map mapping 1 to "row1,col3", 2 to "row2,col3"
 	}
 
 	private static <T> void printCollection(String title, Collection<T> collection) {
